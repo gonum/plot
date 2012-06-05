3dfce4dd45f56101b65499d98f0051c1ea2d3d05
Ethan Burns <burns.ethan@gmail.com>
2012-06-04 22:00:00 -0400

    Oops, forgot to add this file.

---
+++ vg/len.go
@@ -0,0 +1,35 @@
+package vg
+
+const (
+	// ptInch is the number of points in an inch.
+	ptInch = 72
+)
+
+// A Length is a unit-independent representation of length.
+// Internally, the length is stored in postscript points.
+type Length float64
+
+// Points returns a length for the given number of points.
+func Points(pt float64) Length {
+	return Length(pt)
+}
+
+// Inches returns a length for the given numer of inches.
+func Inches(in float64) Length {
+	return Length(in * ptInch)
+}
+
+// Dots returns the length in dots for the given Canvas.
+func (l Length) Dots(c Canvas) float64 {
+	return float64(l) / ptInch * c.DPI()
+}
+
+// Points returns the length in postscript points.
+func (l Length) Points() float64 {
+	return float64(l)
+}
+
+// Inches returns the length in inches.
+func (l Length) Inches() float64 {
+	return float64(l) / ptInch
+}
