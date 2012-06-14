70c6eaab589bc2f38d03cc495721db3f42ee8512
Ethan Burns <burns.ethan@gmail.com>
2012-06-14 08:07:21 -0400

    Minor spacing change.

--- plt/plt_test.go
+++ plt/plt_test.go
@@ -39,15 +39,13 @@ func draw(da *drawArea) {
 		Point{100000, 10},
 		Point{100000.5, 30},
 		Point{100001, 10}))
-	p.AddData(MakeScatter(GlyphStyle{Radius: vg.Points(2), Shape: RingGlyph},
-		Point{100000.5, 15}))
-	p.AddData(MakeScatter(GlyphStyle{Radius: vg.Points(2), Shape: GlyphShape('S')},
-		Point{100000.5, 15}))
 	p.Title.Text = "This is a plot with\ntwo different lines"
 	p.X.Label.Text = "X Label\ngq"
 	p.Y.Min = 10
 	p.Y.Max = 20
-	p.Y.Tick.Marker = ConstantTicks([]Tick{{10, "Ten\n(10)"}, {15, ""}, {20, "Twenty\n(20)"}})
+	p.Y.Tick.Marker = ConstantTicks([]Tick{
+		{10, "Ten\n(10)"}, {15, ""},
+		{20, "Twenty\n(20)"}})
 	p.Y.Label.Text = "Y Label\ngq"
 	p.Draw(da)
 }
