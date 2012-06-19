385bf5444857197abcacbb34788e0b79d65d1715
Ethan Burns <burns.ethan@gmail.com>
2012-06-19 16:58:28 -0400

    Add some examples. Fixes issue 28.

---
+++ plot/example_test.go
@@ -0,0 +1,115 @@
+package plot
+
+import (
+	"code.google.com/p/plotinum/vg"
+	"code.google.com/p/plotinum/vg/veceps"
+	"math/rand"
+)
+
+// An example of making and saving a plot.
+func Example() {
+	// Get some data to plot.
+	pts := make(XYs, 10)
+	for i := range pts {
+		if i == 0 {
+			pts[i].X = rand.Float64()
+		} else {
+			pts[i].X = pts[i-1].X + rand.Float64()
+		}
+		pts[i].Y = rand.Float64()
+	}
+
+	// Make an encapsulated postscript DrawArea
+	w, h := vg.Inches(3), vg.Inches(2)
+	da := NewDrawArea(veceps.New(w, h, "test"), w, h)
+
+	// Make our plot and set some labels.
+	p, err := New()
+	if err != nil {
+		panic(err)
+	}
+	p.Title.Text = "Plot Title"
+	p.X.Label.Text = "X Values"
+	p.Y.Label.Text = "Y Values"
+	line := Line{pts, DefaultLineStyle}
+	scatter := Scatter{pts, DefaultGlyphStyle}
+	p.AddData(line, scatter)
+	p.Legend.AddEntry("line", line, scatter)
+	p.Legend.Top = true
+	p.Draw(da)
+
+	// Save to test.eps
+	err = da.Canvas.(*veceps.Canvas).Save("test.eps")
+	if err != nil {
+		panic(err)
+	}
+}
+
+// An example of making a box plot.
+func Example_boxPlot(da *DrawArea) {
+	// Get some data to plot.
+	n := 10
+	uniform := make(Ys, n)
+	normal := make(Ys, n)
+	expon := make(Ys, n)
+	for i := 0; i < n; i++ {
+		uniform[i] = rand.Float64()
+		normal[i] = rand.NormFloat64()
+		expon[i] = rand.ExpFloat64()
+	}
+
+	// Make our plot and set some labels.
+	p, err := New()
+	if err != nil {
+		panic(err)
+	}
+	p.Title.Text = "Plot Title"
+	p.Y.Label.Text = "Values"
+
+	// Make boxes for our data and add them to the plot.
+	p.AddData(NewBox(vg.Points(20), 0, uniform),
+		NewBox(vg.Points(20), 1, normal),
+		NewBox(vg.Points(20), 2, expon))
+
+	// Set the X axis of the plot to nominal with
+	// the given names for x=0, x=1 and x=2.
+	p.NominalX("Uniform\nDistribution", "Normal\nDistribution",
+		"Exponential\nDistribution")
+
+	p.Draw(da)
+}
+
+// An example of making a horizontal box plot.
+func Example_horizontalBoxes(da *DrawArea) {
+	// Get some data to plot.
+	n := 10
+	uniform := make(Ys, n)
+	normal := make(Ys, n)
+	expon := make(Ys, n)
+	for i := 0; i < n; i++ {
+		uniform[i] = rand.Float64()
+		normal[i] = rand.NormFloat64()
+		expon[i] = rand.ExpFloat64()
+	}
+
+	// Make our plot and set some labels.
+	p, err := New()
+	if err != nil {
+		panic(err)
+	}
+	p.Title.Text = "Plot Title"
+	p.X.Label.Text = "Values"
+
+	// Make horizontal boxes for our data and add
+	// them to the plot.
+	p.AddData(MakeHorizBox(vg.Points(20), 0, uniform),
+		MakeHorizBox(vg.Points(20), 1, normal),
+		MakeHorizBox(vg.Points(20), 2, expon))
+
+	// Set the Y axis of the plot to nominal with
+	// the given names for y=0, y=1 and y=2.
+	p.NominalY("Uniform\nDistribution", "Normal\nDistribution",
+		"Exponential\nDistribution")
+
+	p.Draw(da)
+}
--- plot/plot_test.go
+++ plot/plot_test.go
@@ -4,15 +4,11 @@ import (
 	"code.google.com/p/plotinum/vg"
 	"code.google.com/p/plotinum/vg/veceps"
 	"code.google.com/p/plotinum/vg/vecimg"
-	"math/rand"
-	//	"time"
 	"testing"
 )
 
-var seed = int64(0) // time.Now().UnixNano()
-
 func TestDrawImage(t *testing.T) {
-	w, h := vg.Inches(4), vg.Inches(2)
+	w, h := vg.Inches(3), vg.Inches(2)
 	img, err := vecimg.New(w, h)
 	if err != nil {
 		t.Error(err)
@@ -26,7 +22,7 @@ func TestDrawImage(t *testing.T) {
 }
 
 func TestDrawEps(t *testing.T) {
-	w, h := vg.Inches(4), vg.Inches(2)
+	w, h := vg.Inches(3), vg.Inches(2)
 	da := NewDrawArea(veceps.New(w, h, "test"), w, h)
 	draw(da)
 	err := da.Canvas.(*veceps.Canvas).Save("test.eps")
@@ -37,38 +33,5 @@ func TestDrawEps(t *testing.T) {
 
 // draw draws a simple test plot
 func draw(da *DrawArea) {
-	rand.Seed(seed)
-	n := 10
-	uniform := make(Ys, n)
-	normal := make(Ys, n)
-	expon := make(Ys, n)
-	for i := 0; i < n; i++ {
-		uniform[i] = rand.Float64()
-		normal[i] = rand.NormFloat64()
-		expon[i] = rand.ExpFloat64()
-	}
-	p, err := New()
-	if err != nil {
-		panic(err)
-	}
-	p.Title.Text = "Plot Title"
-	p.X.Label.Text = "Values"
-
-	b0 := MakeHorizBox(vg.Points(20), 0, uniform)
-	b1 := MakeHorizBox(vg.Points(20), 1, normal)
-	b2 := MakeHorizBox(vg.Points(20), 2, expon)
-	p.AddData(b0, b1, b2)
-	p.Legend.AddEntry("outliers", b0.GlyphStyle)
-	p.NominalY("Uniform\nDistribution", "Normal\nDistribution",
-		"Exponential\nDistribution")
-
-	_, med0, _, _ := b0.Statistics()
-	_, med1, _, _ := b1.Statistics()
-	_, med2, _, _ := b2.Statistics()
-	meds := XYs{{med0, b0.X}, {med1, b1.X}, {med2, b2.X}}
-	l := Line{meds, DefaultLineStyle}
-	s := Scatter{meds, GlyphStyle{Shape: CircleGlyph, Radius: vg.Points(2)}}
-	p.AddData(l, s)
-	p.Legend.AddEntry("median", l, s)
-	p.Draw(da)
+	Example_boxPlot(da)
 }
