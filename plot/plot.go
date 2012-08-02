7d999853d1a9740a280004f1fe3f2619d62fec82
Ethan Burns <burns.ethan@gmail.com>
2012-08-02 08:12:31 -0400

    Change a comment.

--- plot/plot.go
+++ plot/plot.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by an MIT-style license
 // that can be found in the LICENSE file.
 
-// plot provides an API for building and drawing plots in Go.
+// plot provides an API for building and drawing plots.
 package plot
 
 import (
