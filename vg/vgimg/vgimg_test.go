e83a11d7f03f842473ff6aee616cb75f0d625e19
btracey <tracey.brendan@gmail.com>
2015-03-25 11:29:10 -0700

    Fix vgimage translate.
    The old code created vg canvases which were incorrectly offset on the canvas. This makes the correct offset.
    
    Fixes Issue 179.
    
    Add testdata folder

---
+++ vg/vgimg/vgimg_test.go
@@ -0,0 +1,48 @@
+package vgimg_test
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/gonum/plot"
+	"github.com/gonum/plot/plotter"
+	"github.com/gonum/plot/vg"
+	"github.com/gonum/plot/vg/draw"
+	"github.com/gonum/plot/vg/vgimg"
+)
+
+func TestIssue179(t *testing.T) {
+	scatter, err := plotter.NewScatter(plotter.XYs{{1, 1}, {0, 1}, {0, 0}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	p, err := plot.New()
+	if err != nil {
+		log.Fatal(err)
+	}
+	p.Add(scatter)
+	p.HideAxes()
+
+	c := vgimg.JpegCanvas{Canvas: vgimg.New(5.08*vg.Centimeter, 5.08*vg.Centimeter)}
+	p.Draw(draw.New(c))
+	b := bytes.NewBuffer([]byte{})
+	if _, err = c.WriteTo(b); err != nil {
+		t.Error(err)
+	}
+
+	f, err := os.Open(filepath.Join("testdata", "issue179.jpg"))
+	if err != nil {
+		t.Error(err)
+	}
+	want, err := ioutil.ReadAll(f)
+	if err != nil {
+		t.Error(err)
+	}
+	if !bytes.Equal(b.Bytes(), want) {
+		t.Error("Image mismatch")
+	}
+}
