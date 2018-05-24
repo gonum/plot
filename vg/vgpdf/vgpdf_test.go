604c5336fef5b903e768b682bef34780bd56a41c
Sebastien Binet <binet@cern.ch>
2018-05-24 10:12:27 +0200

    vg/vgpdf: add example + tests for EmbedFonts

---
+++ vg/vgpdf/vgpdf_test.go
@@ -0,0 +1,118 @@
+// Copyright ©2018 The gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package vgpdf_test
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"testing"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/internal/cmpimg"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg/draw"
+	"gonum.org/v1/plot/vg/vgpdf"
+)
+
+// ExampleEmbedFonts shows how one can embed (or not) fonts inside
+// a PDF plot.
+func ExampleEmbedFonts() {
+	p, err := plot.New()
+	if err != nil {
+		log.Fatalf("could not create plot: %v", err)
+	}
+
+	pts := plotter.XYs{{0, 0}, {0, 1}, {1, 0}, {1, 1}}
+	line, err := plotter.NewLine(pts)
+	if err != nil {
+		log.Fatalf("could not create line: %v", err)
+	}
+	p.Add(line)
+	p.X.Label.Text = "X axis"
+	p.Y.Label.Text = "Y axis"
+
+	c := vgpdf.New(100, 100)
+
+	// enable/disable embedding fonts
+	c.EmbedFonts(true)
+	p.Draw(draw.New(c))
+
+	f, err := os.Create("testdata/enable-embedded-fonts.pdf")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	_, err = c.WriteTo(f)
+	if err != nil {
+		log.Fatalf("could not write canvas: %v", err)
+	}
+
+	err = f.Close()
+	if err != nil {
+		log.Fatalf("could not save canvas: %v", err)
+	}
+}
+
+func TestEmbedFonts(t *testing.T) {
+	for _, tc := range []struct {
+		name  string
+		embed bool
+	}{
+		{
+			name:  "testdata/disable-embedded-fonts_golden.pdf",
+			embed: false,
+		},
+		{
+			name:  "testdata/enable-embedded-fonts_golden.pdf",
+			embed: true,
+		},
+	} {
+		t.Run(fmt.Sprintf("embed=%v", tc.embed), func(t *testing.T) {
+			p, err := plot.New()
+			if err != nil {
+				t.Fatalf("could not create plot: %v", err)
+			}
+
+			pts := plotter.XYs{{0, 0}, {0, 1}, {1, 0}, {1, 1}}
+			line, err := plotter.NewLine(pts)
+			if err != nil {
+				t.Fatalf("could not create line: %v", err)
+			}
+			p.Add(line)
+			p.X.Label.Text = "X axis"
+			p.Y.Label.Text = "Y axis"
+
+			c := vgpdf.New(100, 100)
+
+			// enable/disable embedding fonts
+			c.EmbedFonts(tc.embed)
+			p.Draw(draw.New(c))
+
+			var buf bytes.Buffer
+			_, err = c.WriteTo(&buf)
+			if err != nil {
+				t.Fatalf("could not write canvas: %v", err)
+			}
+
+			want, err := ioutil.ReadFile(tc.name)
+			if err != nil {
+				t.Fatalf("failed to read golden plot: %v", err)
+			}
+
+			ok, err := cmpimg.Equal("pdf", buf.Bytes(), want)
+			if err != nil {
+				t.Fatalf("failed to run cmpimg test: %v", err)
+			}
+
+			if !ok {
+				t.Fatalf("plot mismatch: %v", tc.name)
+			}
+		})
+	}
+}
