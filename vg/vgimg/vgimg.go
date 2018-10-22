d49b649b2e22d47b760ca1fc63f074618f6c64a5
Miguel Pires <miguelpires94@gmail.com>
2018-10-22 14:34:27 +0100

    Reverted gofmt changes

--- vg/vgimg/vgimg.go
+++ vg/vgimg/vgimg.go
@@ -101,7 +101,6 @@ func NewWith(o ...option) *Canvas {
 		c.gc.Scale(1, -1)
 		c.gc.Translate(0, -h)
 	}
-
 	draw.Draw(c.img, c.img.Bounds(), image.Transparent, image.ZP, draw.Src)
 	c.color = []color.Color{color.Black}
 	vg.Initialize(c)
