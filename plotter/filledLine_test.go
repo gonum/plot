8e741a155b598c61128ce411ccad6778dac4c2e3
Igor Mikushkin <igor.mikushkin@gmail.com>
2018-12-20 00:58:26 +0400

    Creating a standalone test for line clipping

--- plotter/filledLine_test.go
+++ plotter/filledLine_test.go
@@ -16,7 +16,7 @@ import (
 	"gonum.org/v1/plot/plotter"
 )
 
-func ExampleLine_filledLine() {
+func clippedFilledLine() {
 	rnd := rand.New(rand.NewSource(1))
 
 	// randomPoints returns some random x, y points
@@ -51,7 +51,49 @@ func ExampleLine_filledLine() {
 
 	p.Add(filled)
 	// testing clipping
+	p.X.Min, p.X.Max = 1, 3
 	p.Y.Max = -1
+
+	err = p.Save(200, 200, "testdata/clippedFilledLine.png")
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+func ExampleLine_filledLine() {
+	rnd := rand.New(rand.NewSource(1))
+
+	// randomPoints returns some random x, y points
+	// with some interesting kind of trend.
+	randomPoints := func(n int, x float64) plotter.XYs {
+		pts := make(plotter.XYs, n)
+		for i := range pts {
+			if i == 0 {
+				pts[i].X = x + rnd.Float64()
+			} else {
+				pts[i].X = pts[i-1].X + 0.5 + rnd.Float64()
+			}
+			pts[i].Y = -5. + 10*rnd.Float64()
+		}
+		return pts
+	}
+
+	p, err := plot.New()
+	if err != nil {
+		log.Panic(err)
+	}
+	p.Title.Text = "Filled Line Example"
+	p.X.Label.Text = "X"
+	p.Y.Label.Text = "Y"
+	p.Add(plotter.NewGrid())
+
+	filled, err := plotter.NewLine(randomPoints(4, 0))
+	if err != nil {
+		log.Panic(err)
+	}
+	filled.FillColor = color.RGBA{R: 196, G: 255, B: 196, A: 255}
+
+	p.Add(filled)
 
 	err = p.Save(200, 200, "testdata/filledLine.png")
 	if err != nil {
@@ -61,4 +103,5 @@ func ExampleLine_filledLine() {
 
 func TestFilledLine(t *testing.T) {
 	cmpimg.CheckPlot(ExampleLine_filledLine, t, "filledLine.png")
+	cmpimg.CheckPlot(clippedFilledLine, t, "clippedFilledLine.png")
 }
