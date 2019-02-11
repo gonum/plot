b99b24273ab44c4e25d128a1cdb30df1dee378b9
Sebastien Binet <binet@cern.ch>
2019-02-11 11:12:58 +0100

    vg/vgpdf: implement multipage PDFs
    Fixes gonum/plot#450.

--- vg/vgpdf/vgpdf_test.go
+++ vg/vgpdf/vgpdf_test.go
@@ -7,6 +7,7 @@ package vgpdf_test
 import (
 	"bytes"
 	"fmt"
+	"image/color"
 	"io/ioutil"
 	"log"
 	"os"
@@ -15,6 +16,7 @@ import (
 	"gonum.org/v1/plot"
 	"gonum.org/v1/plot/internal/cmpimg"
 	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
 	"gonum.org/v1/plot/vg/draw"
 	"gonum.org/v1/plot/vg/vgpdf"
 )
@@ -168,4 +170,55 @@ func TestArc(t *testing.T) {
 	if !ok {
 		t.Fatalf("plot mismatch")
 	}
+}
+
+// Example_multipage shows how one can create a PDF with multiple pages.
+func Example_multipage() {
+	c := vgpdf.New(5*vg.Centimeter, 5*vg.Centimeter)
+
+	for i, col := range []color.RGBA{{B: 255, A: 255}, {R: 255, A: 255}} {
+		if i > 0 {
+			// Add a new page.
+			c.NextPage()
+		}
+
+		p, err := plot.New()
+		if err != nil {
+			log.Fatalf("could not create plot: %v", err)
+		}
+
+		pts := plotter.XYs{{0, 0}, {0, 1}, {1, 0}, {1, 1}}
+		line, err := plotter.NewLine(pts)
+		if err != nil {
+			log.Fatalf("could not create line: %v", err)
+		}
+		line.Color = col
+		p.Add(line)
+		p.Title.Text = fmt.Sprintf("Plot %d", i+1)
+		p.X.Label.Text = "X axis"
+		p.Y.Label.Text = "Y axis"
+
+		// Write plot to page.
+		p.Draw(draw.New(c))
+	}
+
+	f, err := os.Create("testdata/multipage.pdf")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	_, err = c.WriteTo(f)
+	if err != nil {
+		log.Fatalf("could not write canvas: %v", err)
+	}
+
+	err = f.Close()
+	if err != nil {
+		log.Fatalf("could not save canvas: %v", err)
+	}
+}
+
+func TestMultipage(t *testing.T) {
+	cmpimg.CheckPlot(Example_multipage, t, "multipage.pdf")
 }
