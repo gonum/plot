6aa382cc22c2588f2a14dc6cda251d02f5ee3cf0
Sebastien Binet <binet@cern.ch>
2020-04-10 15:15:12 +0200

    vg/vgtex: add filled glyphs to scatter example

--- vg/vgtex/example_canvas_test.go
+++ vg/vgtex/example_canvas_test.go
@@ -18,26 +18,38 @@ import (
 
 // An example of making a LaTeX plot.
 func Example() {
-	scatter, err := plotter.NewScatter(plotter.XYs{{X: 1, Y: 1}, {X: 0, Y: 1}, {X: 0, Y: 0}})
-	if err != nil {
-		log.Fatal(err)
-	}
-	scatter.Color = color.RGBA{R: 255, A: 200}
 	p, err := plot.New()
 	if err != nil {
 		log.Fatal(err)
 	}
-	p.Add(scatter)
 	// p.HideAxes()
 	p.Title.Text = `A scatter plot: $\sqrt{\frac{e^{3i\pi}}{2\cos 3\pi}}$`
 	p.Title.TextStyle.Font.Size = 16
 	p.X.Label.Text = `$x = \eta$`
 	p.Y.Label.Text = `$y$ is some $\Phi$`
 
+	scatter1, err := plotter.NewScatter(plotter.XYs{{X: 1, Y: 1}, {X: 0, Y: 1}, {X: 0, Y: 0}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	scatter1.Color = color.RGBA{R: 255, A: 200}
+
+	scatter2, err := plotter.NewScatter(plotter.XYs{{X: 1, Y: 0}, {X: 1, Y: 0.5}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	scatter2.GlyphStyle.Shape = draw.PyramidGlyph{}
+	scatter2.GlyphStyle.Radius = 2
+	scatter2.Color = color.RGBA{B: 255, A: 200}
+
+	p.Add(scatter1, scatter2)
+
 	txtFont := p.X.Label.TextStyle.Font
 
 	c := vgtex.NewDocument(5*vg.Centimeter, 5*vg.Centimeter)
 	p.Draw(draw.New(c))
+
+	c.SetColor(color.Black)
 	c.FillString(txtFont, vg.Point{X: 2.5 * vg.Centimeter, Y: 2.5 * vg.Centimeter}, "x")
 
 	f, err := os.Create("testdata/scatter.tex")
