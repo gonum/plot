6df2c69fd8455859209e0c3e771d576004f470dd
David Kleiven <davidkleiven446@gmail.com>
2020-08-03 08:55:15 +0200

    fix: Update copyright header, remove commented code

--- plotter/example_rasterheat_test.go
+++ plotter/example_rasterheat_test.go
@@ -1,4 +1,4 @@
-// Copyright ©2015 The Gonum Authors. All rights reserved.
+// Copyright ©2020 The Gonum Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
@@ -39,7 +39,7 @@ func ExampleRasterHeatMap() {
 	plt.Add(raster)
 	plt.Draw(dc)
 	w, err := os.Create("testdata/rasterHeatMap.png")
-	//w, err := os.Create("/home/davidkleiven/rasterHeatMap.png")
+
 	if err != nil {
 		log.Panic(err)
 	}
