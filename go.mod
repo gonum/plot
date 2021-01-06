30cfce8f45fc28b07c7a797d9e15dcdfd0e2e694
Sebastien Binet <binet@cern.ch>
2021-01-06 14:49:09 +0100

    text: add LaTeX example

--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module gonum.org/v1/plot
 go 1.13
 
 require (
-	gioui.org v0.0.0-20201217203518-74cee277ca60
+	gioui.org v0.0.0-20210106084211-c030065af7bc
 	github.com/ajstarks/svgo v0.0.0-20180226025133-644b8db467af
 	github.com/fogleman/gg v1.3.0
-	github.com/go-latex/latex v0.0.0-20201211173324-01eae8bd88f2
+	github.com/go-latex/latex v0.0.0-20201220152147-94de1316b515
 	github.com/phpdave11/gofpdf v1.4.2
 	golang.org/x/exp v0.0.0-20191002040644-a1355ae1e2c3
 	golang.org/x/image v0.0.0-20201208152932-35266b937fa6
---
+++ text/latex_example_test.go
@@ -0,0 +1,84 @@
+// Copyright ©2021 The Gonum Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package text_test
+
+import (
+	"image/color"
+	"log"
+	"math"
+	"testing"
+
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/cmpimg"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/text"
+	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
+)
+
+func ExampleLatex() {
+	plot.DefaultTextHandler = text.Latex{}
+
+	p, err := plot.New()
+	if err != nil {
+		log.Fatalf("could not create plot: %+v", err)
+	}
+
+	p.Title.Text = `$f(x) = \sqrt{\alpha x \Gamma}$`
+	p.X.Label.Text = `$\frac{\sqrt{x}}{2\pi\Gamma\gamma}$`
+	p.Y.Label.Text = `$\beta$`
+
+	p.X.Min = -1
+	p.X.Max = +1
+	p.Y.Min = -1
+	p.Y.Max = +1
+
+	labels, err := plotter.NewLabels(plotter.XYLabels{
+		XYs: []plotter.XY{
+			{X: +0.0, Y: +0.0},
+			{X: -0.9, Y: -0.9},
+			{X: +0.6, Y: +0.0},
+			{X: +0.5, Y: -0.9},
+		},
+		Labels: []string{
+			`$\frac{\sqrt{x}}{2\pi\Gamma\gamma}$`,
+			`$LaTeX$`,
+			"plain",
+			`$\frac{\sqrt{x}}{2\beta}$`,
+		},
+	})
+	if err != nil {
+		log.Fatalf("could not create labels: %+v", err)
+	}
+	labels.TextStyle[0].Font.Size = 24
+	labels.TextStyle[0].Color = color.RGBA{B: 255, A: 255}
+	labels.TextStyle[0].XAlign = draw.XCenter
+	labels.TextStyle[0].YAlign = draw.YCenter
+
+	labels.TextStyle[1].Font.Size = 24
+	labels.TextStyle[1].Color = color.RGBA{R: 255, A: 255}
+	labels.TextStyle[1].Rotation = math.Pi / 4
+
+	labels.TextStyle[2].Font.Size = 24
+	labels.TextStyle[2].Rotation = math.Pi / 4
+	labels.TextStyle[2].YAlign = draw.YCenter
+	labels.TextStyle[2].Handler = text.Plain{}
+
+	labels.TextStyle[3].Font.Size = 24
+	labels.TextStyle[3].Rotation = math.Pi / 2
+
+	p.Add(labels)
+	p.Add(plotter.NewGlyphBoxes())
+	p.Add(plotter.NewGrid())
+
+	err = p.Save(10*vg.Centimeter, 5*vg.Centimeter, "testdata/latex.png")
+	if err != nil {
+		log.Fatalf("could not save plot: %+v", err)
+	}
+}
+
+func TestLatex(t *testing.T) {
+	cmpimg.CheckPlot(ExampleLatex, t, "latex.png")
+}
