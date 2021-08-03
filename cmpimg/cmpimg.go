92ca2ebc87133af4c4de4ec73c49e9b5bc707e4e
Sebastien Binet <binet@cern.ch>
2021-08-03 15:11:18 +0200

    PS1-merge-16bd0a1

--- cmpimg/cmpimg.go
+++ cmpimg/cmpimg.go
@@ -40,6 +40,8 @@ func Equal(typ string, raw1, raw2 []byte) (bool, error) {
 // and returns whether the two images are equal or not.
 //
 // EqualApprox may return an error if the decoding of the raw image somehow failed.
+// EqualApprox only uses the normalized delta parameter for "jpeg", "jpg", "png",
+// and "tiff" images. It ignores that parameter for other document types.
 func EqualApprox(typ string, raw1, raw2 []byte, delta float64) (bool, error) {
 	switch {
 	case delta < 0:
