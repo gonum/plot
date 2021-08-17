e5c0dd480b9f0b27a784983756880591c75a0b6e
Sebastien Binet <binet@cern.ch>
2021-08-17 09:04:23 +0200

    all: add go:build directives

--- vgall.go
+++ vgall.go
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+//go:build !minimal
 // +build !minimal
 
 package plot // import "gonum.org/v1/plot"
