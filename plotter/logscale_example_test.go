d718b1825109c9442a3ad1ae76e6e94bbb932166
Sebastien Binet <binet@cern.ch>
2023-06-05 13:41:22 +0200

    all: add AutoRescale to Axis
    This CL adds a new field to Axis to allow for a better interaction
    between a plot, an axis and its ticker, in order to automatically adapt
    the range of a given axis.
    
    Signed-off-by: Sebastien Binet <binet@cern.ch>

--- plotter/logscale_example_test.go
+++ plotter/logscale_example_test.go
@@ -41,3 +41,34 @@ func Example_logScale() {
 		log.Panic(err)
 	}
 }
+
+// Example_logScaleWithAutoRescale shows how to create a plot with a log-scale on the Y-axis.
+// The Y-axis is instructed to automatically adapt its range according to
+// the underlying Y-ticker.
+func Example_logScaleWithAutoRescale() {
+	p := plot.New()
+	p.Title.Text = "My Plot"
+	p.Y.Scale = plot.LogScale{}
+	p.Y.Tick.Marker = plot.LogTicks{Prec: -1}
+	p.Y.AutoRescale = true
+	p.X.Label.Text = "x"
+	p.Y.Label.Text = "f(x)"
+
+	f := plotter.NewFunction(math.Exp)
+	f.XMin = 0.2
+	f.XMax = 10
+	f.Color = color.RGBA{R: 255, A: 255}
+
+	p.Add(f, plotter.NewGrid())
+	p.Legend.Add("exp(x)", f)
+
+	p.X.Min = f.XMin
+	p.X.Max = f.XMax
+	p.Y.Min = math.Exp(f.XMin)
+	p.Y.Max = math.Exp(f.XMax)
+
+	err := p.Save(10*vg.Centimeter, 10*vg.Centimeter, "testdata/logscale_autorescale.png")
+	if err != nil {
+		log.Panic(err)
+	}
+}
